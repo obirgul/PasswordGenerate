416f7f68fa90a2b1b6f01e1a465c0825a0880bc2
orcunb <orcunbirgul@gmail.com>
2019-02-03 17:01:56 +0300

    unnecessary lines deleted

--- PasswordGenerate.py
+++ PasswordGenerate.py
@@ -1,8 +1,7 @@
-import random, os
+import random
 a= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','{',':','>','?','}','|','!','@','#','$','%','^','&','*','(',')','-','+']
 
 z = int(input("How many words in your password?: "))
-#os.system("clear")
 List = []
 for x in range(z):
     List.append(random.choice(a))
